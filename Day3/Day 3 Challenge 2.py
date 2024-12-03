a87d86bcf9fb3c723f0c2baac373d3157c459b6b
Kylejones01340 <56350720+Rusty942@users.noreply.github.com>
2024-12-03 23:15:12 +0000

    Day 3 challenge 2 catches do to do edge cases

--- Day3/Day 3 Challenge 2.py
+++ Day3/Day 3 Challenge 2.py
@@ -6,7 +6,7 @@ with open('memory.txt', 'r') as file:
 doSplit= memory.split("do()")
 dontSplit = memory.split("don't()")
 
-if (doSplit[0] > dontSplit[0]):
+if (len(doSplit[0]) > len(dontSplit[0])):
     start = dontSplit[0]
 else:
     start = doSplit[0]
@@ -14,20 +14,35 @@ else:
 do = str(re.escape("do()"))
 dont =  str(re.escape("don't()"))
 dos = re.findall(f'{do}(.*?){dont}', memory)
+extraDos = re.findall(f'{do}(?!.*{dont}).*?{do}', memory)
 
 string = start
 for x in range (len(dos)):
     string += dos[x]
+extraString = ""
+for z in range (len(extraDos)):
+    extraString += extraDos[z]
 correctMemories = re.findall("mul\(\d+,\d+\)", string)
+extraCorrectMemories = re.findall("mul\(\d+,\d+\)", extraString)
 
 correctNums = []
 for x in range (len(correctMemories)):
     nums = re.findall("\d+", correctMemories[x])   
     correctNums.append(nums)
+
+extraCorrectNums = []
+for x in range (len(extraCorrectMemories)):
+    extraNums = re.findall("\d+", extraCorrectMemories[x])   
+    extraCorrectNums.append(extraNums)
     
 total = 0
 for y in range (len(correctNums)):
     multi = (int((correctNums[y][0])) * int((correctNums[y][1])))  
     total += multi
+
+for y in range (len(extraCorrectNums)):
+    multi = (int((extraCorrectNums[y][0])) * int((extraCorrectNums[y][1])))  
+    total += multi
+
 
 print (total)  
